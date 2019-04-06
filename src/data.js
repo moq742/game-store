7c831f609652a535e2371bc740ef303e6cba9962
moq742 <moq742@mail.usask.ca>
2019-04-05 23:42:13 -0600

    added the proper description of each game to view for the clients.

--- src/data.js
+++ src/data.js
@@ -11,7 +11,7 @@ export const storeProducts = [
     price: 49.99,
     company: "ThinkGeek",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Minecraft is a game devoid of quests and guided adventures. The world of Minecraft exists for you to build it and transform it into anything and everything imaginable. It operates on a 20 minute day/night cycle, with 10 minutes of daytime, 1.5 minutes at sunset and sunrise, and 7 minutes of nighttime. During nighttime, the world is covered in darkness, allowing hostile mobs to spawn on the surface.",
     inCart: false,
     count: 0,
     total: 0
@@ -23,7 +23,7 @@ export const storeProducts = [
     price: 69.99,
     company: "EA Sports",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "FIFA 19 is a football simulation video game developed by EA Vancouver as part of Electronic Arts' FIFA series. Announced on 6 June 2018 for its E3 2018 press conference, it was released on 28 September 2018 for PlayStation 4. It is the 26th installment in the FIFA series. As with FIFA 18, Cristiano Ronaldo appears as the cover athlete of the regular edition. The game features the UEFA club competitions for the first time, including the UEFA Champions League. Martin Tyler and Alan Smith return as regular commentators, while the new commentary team of Derek Rae and Lee Dixon feature in the UEFA competitions mode. Composer Hans Zimmer and rapper Vince Staples recorded a new remix of the UEFA Champions League anthem specifically for the game. The character Alex Hunter, who first appeared in FIFA 17 returns for the third and final installment of 'The Journey', entitled, 'The Journey: Champions'.",
     inCart: false,
     count: 0,
     total: 0
@@ -35,7 +35,7 @@ export const storeProducts = [
     price: 49.99,
     company: "Epic games",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "The single-player or co-operative mode (played with friends) involves fighting off zombie-like creatures. But Fortnite’s most popular mode is its standalone free-to-play multiplayer platform, Fortnite Battle Royale, in which up to 100 players enter an online game, competing individually or as part of squads of up to four, to be the last player standing within a ever-decreasing battle arena.",
     inCart: false,
     count: 0,
     total: 0
@@ -47,7 +47,7 @@ export const storeProducts = [
     price: 69.99,
     company: "Activision",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Infinite Warfare delivers three unique game modes: Campaign, Multiplayer, and Zombies. In Campaign, players play as Captain Reyes, a pilot turned Commander, who must lead the remaining coalition forces against a relentless, fanatical enemy, while trying to overcome the deadly, extreme environments of space. Multiplayer combines a fluid momentum based movement system, player focused map design, deep customization, and a brand new combat rig system to create an intense gameplay experience where every second counts. Combat Rigs (Rigs) are the ultimate combat systems. Each Rig is a cutting-edge, tactical combat suit worn by the player and is built for totally different styles of play. Players will also join one of four brand-new Mission Teams to unlock calling cards, camos, emblems, and weapons unique to that team. In Zombies, go back in time to fight the undead in a 1980s amusement park, complete with a plethora of rides, an awesome arcade, and a funky, functioning rollercoaster. Embrace previously beloved aspects of the mode like easter eggs, power ups, and novel weapons while experiencing innovations like brand-new team mechanics, the After Life Arcade, and Fate and Fortune Cards.",
     inCart: false,
     count: 0,
     total: 0
@@ -59,7 +59,7 @@ export const storeProducts = [
     price: 69.99,
     company: "IO Interactive",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "In the game, players assume the role of a hired assassin known as Agent 47, who works for a secret agency that specializes in carrying out assassinations of wealthy and decadent criminals. Missions involve contract killings. The game allows the player try choose their own style of gameplay.",
     inCart: false,
     count: 0,
     total: 0
@@ -71,7 +71,7 @@ export const storeProducts = [
     price: 49.99,
     company: "Square Enix Co., Ltd.",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Rise of the Tomb Raider is a third-person action-adventure game in which players control Lara Croft, who is on a quest to discover the legendary city of Kitezh.",
     inCart: false,
     count: 0,
     total: 0
@@ -83,7 +83,7 @@ export const storeProducts = [
     price: 49.99,
     company: "Naughty Dog",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "The single-player story follows Nathan Drake, a former treasure hunter, who is coaxed out of retirement by estranged older brother Sam and longtime partner Victor Sullivan to search for Henry Every's lost treasure, whilst they battle a mercenary group led by Rafe Adler.",
     inCart: false,
     count: 0,
     total: 0
@@ -95,7 +95,7 @@ export const storeProducts = [
     price: 69.99,
     company: "Sony Interactive Entertainment",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Gran Turismo is a racing game. The player must maneuver a car to compete against artificially intelligent drivers on various race tracks. The game uses two different modes: Arcade Mode and Simulation Mode (Gran Turismo Mode in PAL and Japanese versions). In the arcade mode, the player can freely choose the courses and vehicles they wish to use. Winning races unlocks additional cars and courses.",
     inCart: false,
     count: 0,
     total: 0
@@ -109,7 +109,7 @@ export const detailProduct = {
   price: 49.99,
   company: "ThinkGeek",
   info:
-    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    "Minecraft is a game devoid of quests and guided adventures. The world of Minecraft exists for you to build it and transform it into anything and everything imaginable. It operates on a 20 minute day/night cycle, with 10 minutes of daytime, 1.5 minutes at sunset and sunrise, and 7 minutes of nighttime. During nighttime, the world is covered in darkness, allowing hostile mobs to spawn on the surface.",
   inCart: false,
   count: 0,
   total: 0
